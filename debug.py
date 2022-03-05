e9dd61750557772aeab2219cc073b22722a93625
ErebusTW <95756693+ErebusTW@users.noreply.github.com>
2022-03-05 13:52:47 +0800

    [Add]單步跟蹤調適

---
+++ debug.py
@@ -0,0 +1,11 @@
+#IDE單步調適
+def sum_even(n):
+    result = 0
+    for i in range(0, n, 2):
+        #print(i)
+        #assert(i % 2  == 0)
+        result += i
+    return result
+
+n = sum_even(50)
+print(n)
