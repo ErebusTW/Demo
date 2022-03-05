10c932f8b2782e23a2dfb2b3ac413e4965257e9f
ErebusTW <95756693+ErebusTW@users.noreply.github.com>
2022-03-05 14:30:28 +0800

    [Add]python模塊

---
+++ module_demo.py
@@ -0,0 +1,24 @@
+def sum_even(n):
+    result = 0
+    for i in range(0, n, 2):
+        # print(i)
+        # assert(i % 2  ==1)
+        result += i
+    return result
+
+
+# import datetime
+# print("當前時間是", datetime.datetime.now())
+
+# from datetime import *
+# print("當前時間是", datetime.now())
+
+# from datetime import datetime
+# print("當前時間是", datetime.now())
+
+# 當主程序運行時命名空間是__main__,當模塊運行時命名是模塊級運行空間module_demo
+print(f"當前片段代碼運行所在的命名空間{__name__}")
+
+if __name__ == "__main__":
+    n = sum_even(50)
+    print("sum_even函數測試調適結果", n)
